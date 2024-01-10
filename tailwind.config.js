16b1219970d670c6beb012d3b0c7fa0a7c2d4594
sirok1 <s1r0k.dev@gmail.com>
2024-01-10 15:40:09 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './index.html',
+      './src/**/*.{html,js,tsx}'
+  ],
+  theme: {
+    extend: {
+        animation: {
+          tilt: "tilt 10s infinite linear"
+        },
+        keyframes: {
+            tilt: {
+                "0%, 50%, 100%": {
+                    transform: "rotate(0deg)"
+                },
+                "25%": {
+                    transform: "rotate(1deg)"
+                },
+                "75%": {
+                    transform: "rotate(-1deg)"
+                }
+            }
+        }
+    },
+  },
+  plugins: [
+    require('@codaworks/react-glow/tailwind')
+  ],
+}
+
