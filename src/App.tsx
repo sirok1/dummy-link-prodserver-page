eaeefdb5a791d276bb482631d668ffae4e461978
sirok1 <s1r0k.dev@gmail.com>
2024-01-29 18:31:19 +0300

    add cronicle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import timewebIcon from "./assets/timeweb_icon.png"
 import kumaIcon from "./assets/uptime-kuma_icon.svg"
 import wakatimeIcon from "./assets/wakatime-white.svg"
+import cronicleIcon from "./assets/cronicle-logo.png"
 
 function App() {
 
@@ -131,6 +132,21 @@ function App() {
                        <span className="flex items-center space-x-5">
                            <img src={wakatimeIcon} className="h-6 w-6" alt="host icon"/>
                             <span className="pr-6 text-gray-100">Wakapi</span>
+                       </span>
+                        <span
+                            className="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200">Перейти &rarr;
+                        </span>
+                    </button>
+                </div>
+                <div className="relative group">
+                    <div
+                        className="absolute w-full -inset-0.5 bg-gradient-to-r from-cyan-400 to-indigo-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+                    <button type="submit"
+                            onClick={() => location.href = `https://${import.meta.env.VITE_CRONICLE_LINK}`}
+                            className="relative px-7 w-full py-4 bg-black rounded-lg leading-none flex items-center justify-between divide-x divide-gray-600">
+                       <span className="flex items-center space-x-5">
+                           <img src={cronicleIcon} className="h-6 w-6" alt="host icon"/>
+                            <span className="pr-6 text-gray-100">Cronicle</span>
                        </span>
                         <span
                             className="pl-6 text-indigo-400 group-hover:text-gray-100 transition duration-200">Перейти &rarr;
